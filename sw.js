d52190bf81994c7b5dd184a524af66eaf65b44b1
papyndoumbe392 <pierrendoumbe6@gmail.com>
2026-07-12 18:37:40 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "rudis-v1";
+const CACHE = "rudis-v10";
 const FILES = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
