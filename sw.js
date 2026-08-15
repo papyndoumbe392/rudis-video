0cbb279445e37ebddec9cc07bdcb877015755049
papyndoumbe392 <pierrendoumbe6@gmail.com>
2026-08-15 13:18:44 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // sinon les anciennes copies restent sur l'appareil des clients et ils ne voient
 // jamais les nouveautés. Les caches des versions précédentes sont supprimés
 // automatiquement à l'activation.
-const CACHE = "starclip-v41-8";
+const CACHE = "starclip-v41-9";
 const FILES = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
